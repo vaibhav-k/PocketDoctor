ee06a3f6b938c40b35e9f59eacfb02d12ff1400e
gauravvirmani <virmanigaurav97@gmail.com>
2017-07-15 22:45:39 +0530

    added greet dialog in app.dialog

---
+++ app/dialogs/greet.js
@@ -0,0 +1,21 @@
+module.exports = function(bot) {
+    bot.dialog('/greet', [
+        function (session, args, next) {
+            const lastVisit = session.userData.lastVisit;
+
+            session.send(['Hello!', 'Hi there!', 'Hi!']);
+
+            if (!lastVisit) {
+                session.send('This is PocketDoctor');
+                session.userData = Object.assign({}, session.userData, {
+                    lastVisit: new Date()
+                });
+                session.save();
+            } else {
+                session.send('Glad you\'re back!');
+            }
+
+            session.endDialog('How can I help you?');
+        }
+    ]);
+};
