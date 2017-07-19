2bb778b1273a9f1c1fb2a2335141761afc949576
gauravvirmani <virmanigaurav97@gmail.com>
2017-07-20 00:04:04 +0530

    added get request

--- app.js
+++ app.js
@@ -55,5 +55,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-
+server.get(/.*/, restify.plugins.serveStatic({
+    'directory': '.',
+    'default': 'index.html'
+}));
 
