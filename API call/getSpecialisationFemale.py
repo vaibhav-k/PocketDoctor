64d03e857a0bc8788564365dae613df8d72780f5
k-vaibhav <f2015760@goa.bits-pilani.ac.in>
2017-07-22 21:30:37 +0530

    Update getSpecialisationFemale.py

--- API call/getSpecialisationFemale.py
+++ API call/getSpecialisationFemale.py
@@ -50,14 +50,12 @@ def _loadFromWebService(action, _token, language, healthUrl):
     data = json.loads(response.text)
     return data
 
-def loadPSymtoms(selectedSymptoms, gender, yearOfBirth, _token, language, healthUrl):
+def loadSpecialisation(selectedSymptoms, gender, yearOfBirth, _token, language, healthUrl):
     ret = []
     for ss in selectedSymptoms:
         serializedSymptoms = json.dumps(ss)
-        action = "symptoms/proposed?symptoms=[{0}]&gender={1}&year_of_birth={2}".format(serializedSymptoms, gender.name, yearOfBirth)
-        data = _loadFromWebService(action, _token, language, healthUrl)
-        if data:    
-            ret.append(json.dumps(data))
+        action = "diagnosis/specialisations?symptoms=[{0}]&gender={1}&year_of_birth={2}".format(serializedSymptoms, gender.name, yearOfBirth)
+        ret.append(json.dumps(_loadFromWebService(action, _token, language, healthUrl)))
         return json.dumps(ret)
 
 #start process
@@ -70,5 +68,5 @@ if __name__ == '__main__':
     _printRawOutput = config.pritnRawOutput
     _token = _loadToken(username, password, authUrl)
     lines = read_in()
-    symptoms = loadPSymtoms(lines, Gender.Female, 1988, _token, language, healthUrl)
-    print symptoms
+    spec = loadSpecialisation(lines, Gender.Female, 1988, _token, language, healthUrl)
+    print spec
