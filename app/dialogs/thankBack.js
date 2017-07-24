f5b8842f438a5e841481140cfa90dc4b70dd31ee
k-vaibhav <f2015760@goa.bits-pilani.ac.in>
2017-07-24 17:18:12 +0530

    Update thankBack.js

--- app/dialogs/thankBack.js
+++ app/dialogs/thankBack.js
@@ -1,7 +1,7 @@
 module.exports = function(bot) {
     bot.dialog('/thankBack', [
         function (session, args, next) {
-            session.send(["You're welcome", "Happy to help"]);
+            session.send(["You're welcome", "Happy to help", "Get well soon!"]);
             session.endDialog()
         }
     ]);
