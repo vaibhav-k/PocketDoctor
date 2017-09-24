31d91b4b4b6c113a63e3e27f9c578e4b345503bb
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2017-09-24 19:40:53 +0530

    Completed chatbot

--- SandboxData/Scripts/config.py
+++
@@ -1,6 +0,0 @@
-username = "amhackers2017@yandex.com" # Here enter your API user name
-password = "o6W2EqJn95QeFk4d8" # Here enter your API password
-priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://authservice.priaid.ch/login
-priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://healthservice.priaid.ch
-language = "en-gb" # en-gb, de-ch, fr-fr, es-es, tr-tr, sk-sk...
-pritnRawOutput = False # This flag can be set to see printed json data structure of webservice responses
--- plzREADME.md
+++
@@ -1,6 +0,0 @@
-# PocketDoctor
-Big Data Challenge - July 2017
-
-
-
-Bearer amhackers2017@yandex.com:WiOYqtquWLq0R1TMbtV7hw==
